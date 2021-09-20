f0f7ee3b958b55329c72338799cbb4728f1521a5
FlutLab.io <>
2021-09-20 14:31:25 +0000

    Autosave

--- lib/result.dart
+++ lib/result.dart
@@ -6,8 +6,14 @@ class Result extends StatelessWidget {
   Result({this.score});
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(padding: EdgeInsets.all(10), child: Text("Your score is $score", style: TextStyle(color: Colors.deepOrange, fontSize: 20))),
-    );
+    return Column(children: [
+      Center(
+        child: Container(padding: EdgeInsets.all(10), child: Text("Your score is $score", style: TextStyle(color: Colors.deepOrange, fontSize: 20))),
+      ),
+      ElevatedButton(
+        child: Text('Play again'),
+        onPressed: null,
+      )
+    ]);
   }
 }
