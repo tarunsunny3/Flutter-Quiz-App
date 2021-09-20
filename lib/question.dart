f88b524f7ef5843be7603a48e7d6e4a783404df3
FlutLab.io <>
2021-09-20 15:18:18 +0000

    Autosave

--- lib/question.dart
+++ lib/question.dart
@@ -12,7 +12,7 @@ class Question extends StatelessWidget {
         padding: EdgeInsets.all(10),
         child: Text(
           questionText,
-          style: TextStyle(color: Colors.amber, fontSize: 30),
+          style: TextStyle(color: Colors.blue, fontSize: 30),
           textAlign: TextAlign.center,
         ));
   }
