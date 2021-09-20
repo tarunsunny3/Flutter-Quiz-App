1ede7212829c368f4354973903f706301a657604
FlutLab.io <>
2021-09-20 13:25:14 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.cyanAccent,
+        primarySwatch: Colors.cyan,
       ),
       home: Scaffold(
         appBar: AppBar(
