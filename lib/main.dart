9476444d20528fadedfc3ce1cc216f091225f2fa
FlutLab.io <>
2021-09-20 13:24:46 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,9 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
