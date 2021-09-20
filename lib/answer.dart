ce00d2d105721fe56b4c343e1eba4e3fadd7f406
FlutLab.io <>
2021-09-20 13:29:37 +0000

    Autosave

--- lib/answer.dart
+++ lib/answer.dart
@@ -6,6 +6,6 @@ class Answer extends StatelessWidget {
   Answer(this.answerClickHandler, this.optionText);
   @override
   Widget build(BuildContext context) {
-    return Container(margin: EdgeInsets.only(bottom: 10, left: 5, right: 5), child: ElevatedButton(child: Text(optionText), onPressed: answerClickHandler));
+    return Container(padding: EdgeInsets.all(10), margin: EdgeInsets.only(bottom: 10, left: 5, right: 5), child: ElevatedButton(child: Text(optionText), onPressed: answerClickHandler));
   }
 }
