05e9ecc7239e189fba962fabdc8b462c12f07617
FlutLab.io <>
2021-09-20 14:46:09 +0000

    Autosave

--- lib/answer.dart
+++ lib/answer.dart
@@ -8,7 +8,7 @@ class Answer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.maxFinite,
-      margin: EdgeInsets.only(bottom: 15, left: 30, right: 10),
+      margin: EdgeInsets.only(bottom: 15, left: 20, right: 20),
       child: ElevatedButton(
         child: Padding(
           padding: EdgeInsets.all(10),
